04edbaf94afada53cbef37d02f22990cc52bb06f
rawan187 <rawanakh2005@gmail.com>
2025-03-28 19:40:43 +0200

    enemy
    almost finish

---
+++ Scenes/enemy_w.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const speed = 5
+
+var direction = 1
+
+@onready var right =  $RayCastright
+@onready var left =  $RayCastleft
+@onready var animatedSprite = $AnimatedSprite2D
+
+func _process(delta: float) -> void:
+	if right.is_colliding():
+		direction = -1
+		animatedSprite.flip_h = true
+		
+	if left.is_colliding():
+		direction = 1
+		animatedSprite.flip_h = false
+		
+	velocity.x += direction * speed 
+	move_and_slide()
