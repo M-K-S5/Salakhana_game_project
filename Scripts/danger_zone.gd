907768a29f9a99b02eb3d33fff2ed9eb41fd051f
hadeel <hadeelhamakii25@gmail.com>
2025-03-30 07:26:25 +0200

    fixes
    cleaned enemy code
    boundary danger zone supposedly working now
    added collision area code on fire pit

--- Scripts/danger_zone.gd
+++ Scripts/danger_zone.gd
@@ -1,12 +1,10 @@
-extends Area2D
+extends Node2D
+@onready var timer = $Area2D/Timer
 
-@onready var timer: Timer = $Timer
-
-func _on_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	print ("YOU DIED!!")
 	Engine.time_scale = 0.5
 	timer.start()
-
 
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1.0
