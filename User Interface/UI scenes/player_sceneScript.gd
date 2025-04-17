69a87cfa0c175e592502d972498eeb342274188e
Hzm Saeed <hzmsaeed2000@gmail.com>
2025-04-17 18:56:54 +0200

    fixes + UI script
    1- camera centered
    2- cooldown code implemented
    3- cards deleted (for the tenth time)

--- User Interface/UI scenes/player_sceneScript.gd
+++ User Interface/UI scenes/player_sceneScript.gd
@@ -1,1 +1,38 @@
 extends Control
+
+#-- definitions :
+
+#---
+var dashisready : bool = true
+
+var potionisready : bool = true
+
+
+func _process(delta):
+	var time_left_health : int = $"Health_cooldown/TimerH".time_left
+	var time_left_Dash : int = $"Dash_cooldown/TimerD".time_left
+	$Dash_cooldown.set_value_no_signal(time_left_Dash)
+	$Health_cooldown.set_value_no_signal(time_left_health)
+	
+	if Input.is_action_just_pressed("dash") and dashisready:
+		dashisready = false
+		$"Dash_cooldown/TimerD".start()
+		print ("dash done")
+		
+		
+	if Input.is_action_just_pressed("healPotion") and potionisready:
+		potionisready = false
+		$"Health_cooldown/TimerH".start()
+		print ("heal done")
+		
+	
+
+
+
+
+func _on_timer_timeout() -> void:
+	dashisready = true
+
+
+func _on_timer_h_timeout() -> void:
+	potionisready = true
